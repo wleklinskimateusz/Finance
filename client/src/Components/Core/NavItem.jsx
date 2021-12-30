c6ba0c1eac416c325a3b5a5fe736ab5aa30ea251
Mateusz Wlekliński <matix0508@gmail.com>
2021-12-30 16:30:42 +0100

    switch color while navigating

--- client/src/Components/Core/NavItem.jsx
+++ client/src/Components/Core/NavItem.jsx
@@ -1,9 +1,14 @@
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 
 export function NavItem(props) {
+    let current = useLocation();
+
+    const active = current.pathname.includes(props.to)
+    console.log(current)
+    console.log(props.to)
     return (
         <Link to={props.to} className="btn btn-ghost btn-sm rounded-btn">
-            {props.children}
+            <div className={active ? "text-primary" : ""}>{props.children}</div>
         </Link>
     )
 }
