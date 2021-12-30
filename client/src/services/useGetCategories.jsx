c58a96103fd5faf7c3842d1fbc064219b1b4236a
Mateusz Wlekliński <matix0508@gmail.com>
2021-12-30 18:21:32 +0100

    connected to backend

--- client/src/services/useGetCategories.jsx
+++ client/src/services/useGetCategories.jsx
@@ -1,7 +1,7 @@
 import {useGetExpenses} from "./useGetExpenses";
 
 export function useGetCategories() {
-    const [expenses, expensesState] = useGetExpenses();
+    const [expenses] = useGetExpenses();
     if (expenses)
         return [...new Set(expenses.map(item => item.category))]
     else return []
